f43433b8f1cb76b8eb5662b4d93fdab4736bd98b
Konstantin <kp@hls.tv>
2019-11-28 13:54:20 +0200

    Add sleep

--- test_items.py
+++ test_items.py
@@ -1,3 +1,6 @@
+import time
+
 def test_button_is_displayed(browser):
+	time.sleep(30)
 	button = browser.find_element_by_css_selector('.btn.btn-lg.btn-primary.btn-add-to-basket')
-	assert button.is_displayed()
+	assert button.is_displayed(), "Button is not displayed"
